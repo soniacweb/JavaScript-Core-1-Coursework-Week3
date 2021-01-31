381bad474a220127e9d390f14fdc2cf99eeccb71
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2021-01-31 21:53:28 +0000

    3 exerccises complete

--- 1-exercises/C-array-every/exercise.js
+++ 1-exercises/C-array-every/exercise.js
@@ -5,7 +5,11 @@
 var students = ["Omar", "Austine", "Dany", "Swathi", "Lesley", "Rukmini"];
 var group = ["Austine", "Dany", "Swathi", "Daniel"];
 
-var groupIsOnlyStudents; // complete this statement
+function areTheyStudents(a, b) {
+  const equals = JSON.stringify(a) === JSON.stringify(b);
+  return equals;
+}
+var groupIsOnlyStudents = group.every(areTheyStudents); // complete this statement
 
 if (groupIsOnlyStudents) {
   console.log("The group contains only students");
